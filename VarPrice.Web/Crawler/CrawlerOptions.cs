28a7e153f4b01115b370e80a725761054a5f4d7c
bodomus <bodomus@gmail.com>
2026-02-12 15:25:23 +0200

    ~ decrease amount of items in query.

--- VarPrice.Web/Crawler/CrawlerOptions.cs
+++ VarPrice.Web/Crawler/CrawlerOptions.cs
@@ -4,7 +4,7 @@ public sealed class CrawlerOptions
 {
     public Uri SitemapIndexUrl { get; set; } = new("https://varus.ua/sitemap-index.xml");
     public string VegetablesUrlContains { get; set; } = "";
-    public int MaxProductsPerRun { get; set; } = 200;
+    public int MaxProductsPerRun { get; set; } = 20;
     public int MaxSitemapsToVisit { get; set; } = 200;
     public int MaxUrlsToCollect { get; set; } = 50_000;
     public TimeSpan HttpTimeout { get; set; } = TimeSpan.FromSeconds(30);
--- VarPrice.Web/Crawler/ProductUrlFilter.cs
+++ VarPrice.Web/Crawler/ProductUrlFilter.cs
@@ -3,12 +3,13 @@ namespace VarPrice.Web.Crawler;
 public interface IProductUrlFilter
 {
     bool IsProductUrl(Uri url);
+    bool IsTestUrl(Uri url);
 }
 
 public sealed class VarusProductUrlFilter : IProductUrlFilter
 {
-    private static readonly string[] ProductMarkers = ["/product/", "/tovar/", "/item/", "/p/"];
-    private static readonly string[] BlockMarkers = ["/blog", "/novosti", "/news", "/akcii"];
+    private static readonly string[] ProductMarkers = ["/product/", "/tovar/", "/item/", "/p/", "/shampun"];
+    private static readonly string[] BlockMarkers = ["/blog", "/novosti", "/news", "/akcii", "/test1"];
 
     public bool IsProductUrl(Uri url)
     {
@@ -16,7 +17,16 @@ public sealed class VarusProductUrlFilter : IProductUrlFilter
 
         if (BlockMarkers.Any(path.Contains))
             return false;
-
+        // return true;
         return ProductMarkers.Any(path.Contains);
+        return false;
+    }
+
+    public bool IsTestUrl(Uri url)
+    {
+        var path = url.AbsolutePath.ToLowerInvariant();
+        if (BlockMarkers.Any(path.Contains))
+            return true;
+        return false;
     }
 }
